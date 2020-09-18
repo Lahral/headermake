77fbb956170e126f1bb4976ffbec5ca3a516ba65
Lahral <pendragonheart@gmail.com>
2020-09-18 13:49:48 -0700

    added in getFiles(), now there is a way to figure out what files to add.

--- hmake.py
+++ hmake.py
@@ -7,9 +7,9 @@ today = date.today()
 header = ["/*###########################################","","Written by hmake", "", "###########################################*/"]
 hheader = ["#ifndef ","#define ", "#endif"]
 
-platform = sys.platform()
+platform = sys.platform
 
-def pasteHeaders(cFile,hFile, filename)
+def pasteHeaders(cFile,hFile, filename):
     file = open(cFile, "w")
     tempheader = header
     tempheader[1] = "Filename: " + filename
@@ -25,20 +25,43 @@ def pasteHeaders(cFile,hFile, filename)
     tempheader[3] = "Generated on date: " + today
     file.writelines(tempheader)
     tempdefs = hheader
-    tempdefs[0].append(filename.upper +"_H_")
-    tempdefs[2].append(filename.upper +"_H_")
-    tempdefs[3].append("//" + filename.upper +"_H_")
+    tempdefs[0].append(filename.upper() +"_H_")
+    tempdefs[2].append(filename.upper() +"_H_")
+    tempdefs[3].append("//" + filename.upper() +"_H_")
     file.writelines(tempdefs)
     file.close()
+
+def getFiles():
+    filename = input("Enter filename to generate: ")
+    includes = input("Enter standard headers to include: ").split()
+    dependencies = input("Enter custom headers to include: ").split()
+    generateDeps = input("Generate custom headers?[y/N]")
+    if(generateDeps == "y" && dependencies):
+        makedependencies = True
+    else
+        makedependencies = False
+    directory = input("type in directory path where files are to be created: ")
+    if(not directory):
+        directory = "~/"
+    generateFiles(includes, dependencies, filename, directory, makedependencies)
+
+
+def rungui():
 
 
 def generateFiles(includes, dependencies, filename, directory, makedependencies):
 
     ## generating the includes as well as the empty headers
-    includes = list(map(lambda u: "#include <"+u+">",includes))
-    customIncludes = list(map(lambda u: "#include \""+u+"\"",dependencies))
+    if(not includes)
+        includes = []
+    else:
+        includes = list(map(lambda u: "#include <"+u+">",includes))
+    if(not customIncludes):
+        customIncludes = []
+    else:
+        customIncludes = list(map(lambda u: "#include \""+u+"\"",dependencies))
 
-    if (makedependencies == true):
+    if (makedependencies == True):
         dependentCpp = list(map(lambda x,y: x+'/'+y+'.cpp',[directory]*len(dependencies), dependencies))
         dependentH = list(map(lambda x,y: x+'/'+y+'.cpp',[directory]*len(dependencies), dependencies))
 
@@ -48,14 +71,14 @@ def generateFiles(includes, dependencies, filename, directory, makedependencies)
     #generate the command to make the files:
     if(platform == 'linux'):
         command = "touch" + cFile +"\ntouch" + hFile + "\n"
-        if(makedependencies == true):
+        if(makedependencies == True):
             itr = 0
             while itr < len(dependentCpp):
                 command += "touch" + dependentCpp[itr] + "\ntouch" + dependentH[itr] + "\n"
                 itr += 1
     else:
         command = "type nul >" + cFile + "\r\ntype nul > " + hFile + "\r\n"
-        if(makedependencies == true):
+        if(makedependencies == True):
             itr = 0
             while itr < len(dependentCpp):
                 command += "type nul >" + dependentCpp[itr] + "\r\ntype nul >" + dependentH[itr] + "\r\n"
@@ -65,14 +88,23 @@ def generateFiles(includes, dependencies, filename, directory, makedependencies)
     os.system(command)
 
     pasteHeaders(cFile,hFile,filename)
-    if(makedependencies == true):
+    if(makedependencies == True):
         while itr < len(dependentCpp):
             pasteHeaders(dependentCpp[itr], dependentH[itr], dependencies[itr])
             itr += 1
 
 
 
+def main(gui):
+    if (platform == "darwin"):
+        print("Mac OS not supported")
+        return
+    if(gui == False):
+        getFiles()
+    else
+        rungui()
+
 
 
 if __name__ == '__main__':
-    main()
+    main(False)
